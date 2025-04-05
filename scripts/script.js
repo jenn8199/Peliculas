37fdd9e46559225bfb57331181e5ecab34165b2e
jenn8199 <jennihernandez8199@gmail.com>
2025-04-05 16:43:50 -0600

    agregué la opción para salirse o cerrar sesión que retorna al login

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Este código es para el botón de "Scroll to Top"
 const btn = document.getElementById("scrollTopBtn");
 
 window.onscroll = function () {
@@ -8,8 +9,26 @@ btn.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
+
+//Este código es para el botón de modo oscuro
 const toggleBtn = document.getElementById('darkToggle');
 
 toggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('modo-oscuro');
+});
+
+
+// Este código es para el botón de perfil y el menú desplegable
+const perfilBtn = document.getElementById('perfilBtn');
+const menuPerfil = document.getElementById('menuPerfil');
+
+perfilBtn.addEventListener('click', () => {
+  menuPerfil.style.display = menuPerfil.style.display === 'block' ? 'none' : 'block';
+});
+
+// Opcional: Cierra el menú si haces clic fuera de él
+document.addEventListener('click', (e) => {
+  if (!perfilBtn.contains(e.target) && !menuPerfil.contains(e.target)) {
+    menuPerfil.style.display = 'none';
+  }
 });
